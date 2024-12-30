37a9f35b569c3fb9722c9cf6008bc25e0d4bb8e9
Chaymaa-Ari <chaymaa.ari2002@gmail.com>
2024-12-30 15:21:41 +0100

    Initial commit for gh-pages

--- src/app/models/show.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Show {
-    id?: number; // L'identifiant est optionnel car il est généré par le serveur
-    title: string; // Le titre de la série
-    description: string; // Une description de la série
-    episodes: number; // Le nombre d'épisodes
-  }
-  
